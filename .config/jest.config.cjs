b14256c360abd4a0bded8d23d0f929665d293ab0
Sam Magura <srmagura@gmail.com>
2021-09-29 10:27:09 -0400

    move some config files to .config

--- .config/jest.config.cjs
+++ .config/jest.config.cjs
@@ -1,6 +1,9 @@
+const path = require('path')
+
 module.exports = {
     timers: 'fake',
     resetMocks: true,
 
+    rootDir: path.resolve(__dirname, '..'),
     testMatch: ['**/__tests__/**/*.test.ts?(x)'],
 }
